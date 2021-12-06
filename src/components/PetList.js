00ac186f5f61d02e1967f1d5dd016c32eb976e6d
Amy Posten <amyposten85@gmail.com>
2021-12-06 16:41:31 -0500

    question mark

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -4,7 +4,7 @@ const PetList = ({ allPets, search, handleDelete }) => {
   return (
     <ul className="pets">
       {allPets
-        .filter((pet) => pet.name.toLowerCase().includes(search))
+        .filter((pet) => pet.name?.toLowerCase().includes(search))
         .map((pet) => (
           <PetCard pet={pet} key={pet.id} handleDelete={handleDelete} />
         ))}
